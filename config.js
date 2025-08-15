f7ce4833d98f16a5218e0cd51d48fe7a7c83ccd9
ziddimasoom4046 <ziddimasoom4046@gmail.com>
2025-08-15 15:07:46 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    CAPTION: process.env.CAPTION || "BOT BY ZiDDI MASOOM",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/HlOGU.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "UMAR BOT IS ALIVED RIGHT NOW",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923441585446",
+    OWNERS: [
+  "923441585446@s.whatsapp.net",
+  "923441585446@s.whatsapp.net"
+], // array of owner numbers as strings
+    OWNER_NAME: process.env.OWNER_NAME || "ZidDi MaSOoM",
+    READ_CMD: process.env.READ_CMD || "true",
+    BOT_NAME: process.env.BOT_NAME || "Z I D D I     B O T",
+    STATUS_REPLY: process.env.STATUS_REPLY || "*Seen Done By ZiDDi Bot*",
+    STATUS_REACT: process.env.STATUS_REACT || "true",
+    ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39"// omdbapi.com
+};
